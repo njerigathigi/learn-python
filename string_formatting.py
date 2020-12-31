fd99ae3bb6766acf881cab7b4f34fdcff07103d5
njerigathigi <njeriiterry@gmail.com>
2021-01-01 00:57:40 +0300

    updating notes

---
+++ string_formatting.py
@@ -0,0 +1,7 @@
+# Named Indexes
+
+# You can also use named indexes by entering a name inside the curly brackets 
+# {carname}, but then you must use names when you pass the parameter values 
+# txt.format(carname = "Ford"):
+
+print('i have a {carname} , it is a {model}.'.format(carname = 'Ford', model= 'Mustang'))
