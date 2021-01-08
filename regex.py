c89b99b44177b62849d15353930720a9633acde5
njerigathigi <njeriiterry@gmail.com>
2021-01-08 19:27:24 +0300

    updating notes on special sequences

--- regex.py
+++ regex.py
@@ -119,3 +119,82 @@ if either_all:
     print('match found')
 else:
     print('no match!')
+    
+print()
+
+# Special Sequences
+
+
+# \A	Returns a match if the specified characters are at the beginning of the string	"\AThe"	
+# \b	Returns a match where the specified characters are at the beginning or at the end of a word
+#       (the "r" in the beginning is making sure that the string is being treated as a "raw string")	
+#       r"\bain" r"ain\b"	
+# \B	Returns a match where the specified characters are present, but NOT at the beginning 
+#       (or at the end) of a word
+#       (the "r" in the beginning is making sure that the string is being treated as a "raw string")	
+#       r"\Bain" r"ain\B"	
+# \d	Returns a match where the string contains digits (numbers from 0-9)	"\d"	
+# \D	Returns a match where the string DOES NOT contain digits	"\D"	
+# \s	Returns a match where the string contains a white space character	"\s"	
+# \S	Returns a match where the string DOES NOT contain a white space character	"\S"	
+# \w	Returns a match where the string contains any word characters 
+#       (characters from a to Z, digits from 0-9, and the underscore _ character)	"\w"	
+# \W	Returns a match where the string DOES NOT contain any word characters	"\W"	
+# \Z	Returns a match if the specified characters are at the end of the string	"Spain\Z"	
+
+print()
+text1 = 'The rain in Spain'
+characters_at_beginning_of_str = re.findall('\AThe', text1) #case sensitive. will return empty
+                                                            #list if 'the' in lowercase.
+
+print(characters_at_beginning_of_str)
+
+if characters_at_beginning_of_str:
+    print('match found!')
+else:
+    print('no match')
+
+print()
+characters_at_beginning_of_str_or_end = re.findall(r'\bThe', text1) #r -rawstring .
+characters_at_beginning_of_str_or_end2 = re.findall(r'ain\b',text1) #normally \b - bytes literal 
+
+print(characters_at_beginning_of_str_or_end)
+print(characters_at_beginning_of_str_or_end2)
+print()
+
+
+char_present_but_not_in_beg_or_end = re.findall(r'\Bain', text1)
+char_present_but_not_in_beg_or_end2 = re.findall(r'ain\B', text1)
+print(char_present_but_not_in_beg_or_end)
+print(char_present_but_not_in_beg_or_end2)
+print()
+
+
+str_contains_digits = re.findall('\d', text1)
+print(str_contains_digits)
+print()
+
+str_with_no_digits = re.findall('\D', text1) #returns match at each character thats not a digit.
+print(str_with_no_digits) #list with every non-digit character as item
+print()
+
+str_with_whitespace = re.findall('\s', text1)
+print(str_with_whitespace) #list with every whitespace character as item
+print()
+
+str_without_whitespace = re.findall('\S', text1)
+print(str_without_whitespace) #list with every non-whitespace character as item.
+print()
+
+text2 = 'Nala_cat....,,,,,,,, was my 1.0 st furbaby'
+
+str_with_word_characters = re.findall('\w', text2)
+print(str_with_word_characters) #all letters, numbers and underscore. if floating point , (.) , is ignored.
+print()
+
+str_without_word_character = re.findall('\W', text2)
+print(str_without_word_character)
+print()
+
+characters_at_end_of_str = re.findall('ain\Z', text1)
+print(characters_at_end_of_str)
