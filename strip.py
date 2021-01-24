8afd4cf2313e85ba766b326e0e849f741cebe607
njerigathigi <njeriiterry@gmail.com>
2021-01-24 17:04:04 +0300

    updating notes on rstrip and lstrip

--- strip.py
+++ strip.py
@@ -10,3 +10,36 @@
 txt = ",,,,,rrttgg.....banana....rrr"
 x = txt.strip(",.grt")
 print(x)
+print()
+
+#rstrip
+# The rstrip() method removes any trailing characters 
+# (characters at the end a string) ie on the right side of the string.
+# space is the default trailing character to remove. 
+
+#syntax
+# string.rstrip(characters)
+
+# Parameter	Description
+# characters	Optional. A set of characters to remove as trailing characters
+
+# y = txt.rstrip('rrr')
+# print(y)
+
+sentence = "banana,,,,,ssqqqww....."
+fruit = sentence.rstrip(',sqw.')
+print(fruit)
+
+# lstrip
+# The lstrip() method removes any leading characters ie to the left of the string
+# space is the default leading character to remove
+
+# Syntax
+# string.lstrip(characters)
+
+# Parameter	    Description
+# characters	Optional. A set of characters to remove as leading characters
+
+txt1 = ",,,,,ssaaww.....banana"
+new_txt1 = txt1.lstrip(',saw.')
+print(new_txt1)
